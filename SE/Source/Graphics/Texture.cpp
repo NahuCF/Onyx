d8c6a37bc3c835aa6d16d375dbf883573e5199ff
Nahuel Christian Fernandez <nahuefer105@outlook.com>
2021-09-10 00:19:25 -0300

    Now the texture class only creates a textrue

--- SE/Source/Graphics/Texture.cpp
+++ SE/Source/Graphics/Texture.cpp
@@ -33,57 +33,15 @@ namespace se {
 
 		stbi_image_free(m_TextureData);
 		glBindTexture(GL_TEXTURE_2D, 0);
-
-		//Create VAO
-
-		float vertices[20] =
-		{
-			Width / 2,  Height / 2, 0.0f,  1.0f, 1.0f,  //Top Right
-			Width / 2, -Height / 2, 0.0f,  1.0f, 0.0f,  //Bottom Right
-		   -Width / 2, -Height / 2, 0.0f,  0.0f, 0.0f,  //Bottom Left
-		   -Width / 2,  Height / 2, 0.0f,  0.0f, 1.0f	//Top Left
-		};
-
-		uint32_t indices[6] =
-		{
-			0, 1, 3,
-			1, 2, 3
-		};
-
-		glGenVertexArrays(1, &m_VAO);
-		glGenBuffers(1, &m_VBO);
-		glGenBuffers(1, &m_EBO);
-
-		glBindVertexArray(m_VAO);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(1);
-
-		glBindBuffer(GL_VERTEX_ARRAY, 0);
-		glBindVertexArray(0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	}
 
 	Texture::~Texture()
 	{
-		glDeleteVertexArrays(1, &m_VAO);
-		glDeleteBuffers(1, &m_VBO);
-		glDeleteBuffers(1, &m_EBO);
 	}
 
-	void Texture::UseTexture() const 
+	void Texture::Bind() const
 	{
 		glBindTexture(GL_TEXTURE_2D, m_TextureID);
-		glBindVertexArray(m_VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
 
 }
--- SE/Source/Graphics/Texture.h
+++ SE/Source/Graphics/Texture.h
@@ -9,14 +9,15 @@ namespace se {
 	public:
 		Texture(const char* texturePath, float Width, float Height);
 		~Texture();
-		void UseTexture() const;
+
+		void Bind() const;
+
 		uint32_t GetTextureID() const { return m_TextureID; }
 		lptm::Vector2D GetTextureSize() const { return lptm::Vector2D(m_TextureWidth, m_TextureHeight); }
 	private:
 		uint32_t m_TextureID;
 		unsigned char* m_TextureData;
 		int m_TextureWidth, m_TextureHeight, m_NChannels;
-		uint32_t m_VAO, m_VBO, m_EBO;
 	};
 
 }
