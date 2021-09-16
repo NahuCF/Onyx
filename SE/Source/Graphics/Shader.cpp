b4bfdbf25110b49c84109af546534a211723d553
Nahuel Christian Fernandez <nahuefer105@outlook.com>
2021-09-16 01:34:00 -0300

    Deleted shit code in shader class

--- SE/Source/Graphics/Shader.cpp
+++ SE/Source/Graphics/Shader.cpp
@@ -8,7 +8,6 @@
 namespace se {
 
 	Shader::Shader(const char* vertexPath, const char* fragmentPath)
-		: m_RealXPos(0), m_RealYPos(0), m_CurrentXPos(0), m_CurrentYPos(0)
 	{
 		//Open files
 		m_VShaderFile.open(vertexPath);
@@ -64,13 +63,6 @@ namespace se {
 
 		glDeleteShader(m_VertexID);
 		glDeleteShader(m_FragmentID);
-
-		//Initializating variable
-		Bind();
-		m_DefaultPos = glm::mat4(1.0f);
-		m_DefaultPos = glm::translate(m_DefaultPos, glm::vec3(0.0f, 0.0f, 0.0f));
-		m_RealZPos = 0.0f;
-		glUniformMatrix4fv(glGetUniformLocation(this->m_ProgramID, "move"), 1, GL_FALSE, &m_DefaultPos[0][0]);
 	}
 
 	Shader::~Shader()
@@ -91,104 +83,6 @@ namespace se {
 	void Shader::SetUniform(Shader& shader, const char* transformName, glm::mat4& matrix)
 	{
 		glUniformMatrix4fv(glGetUniformLocation(shader.m_ProgramID, transformName), 1, GL_FALSE, &matrix[0][0]);
-	}
-
-	void Shader::AddShader(std::vector<Shader*> &contenedor)
-	{
-		contenedor.push_back(this);
-	}
-
-	void Shader::MoveRight(std::vector<Shader*>& contenedor, float x)
-	{
-		for(int i = 0; i < contenedor.size(); i++)
-		{
-			contenedor[i]->Bind();
-			contenedor[i]->m_RealXPos += x;
-			contenedor[i]->m_XAxisMovement = glm::mat4(1.0f);
-			contenedor[i]->m_XAxisMovement = glm::translate(contenedor[i]->m_XAxisMovement, glm::vec3(contenedor[i]->m_RealXPos, contenedor[i]->m_RealYPos, contenedor[i]->m_RealZPos));
-			glUniformMatrix4fv(glGetUniformLocation(contenedor[i]->m_ProgramID, "move"), 1, GL_FALSE, &contenedor[i]->m_XAxisMovement[0][0]);
-		}
-		m_CurrentXPos += x;
-	}
-
-	void Shader::MoveLeft(std::vector<Shader*>& contenedor, float x)
-	{
-		for (int i = 0; i < contenedor.size(); i++)
-		{
-			contenedor[i]->Bind();
-			contenedor[i]->m_RealXPos -= x;
-			contenedor[i]->m_XAxisMovement = glm::mat4(1.0f);
-			contenedor[i]->m_XAxisMovement = glm::translate(contenedor[i]->m_XAxisMovement, glm::vec3(contenedor[i]->m_RealXPos, contenedor[i]->m_RealYPos, contenedor[i]->m_RealZPos));
-			glUniformMatrix4fv(glGetUniformLocation(contenedor[i]->m_ProgramID, "move"), 1, GL_FALSE, &contenedor[i]->m_XAxisMovement[0][0]);
-		}
-		m_CurrentXPos -= x;
-	}
-
-	void Shader::MoveUp(std::vector<Shader*>& contenedor, float y)
-	{
-		for (int i = 0; i < contenedor.size(); i++)
-		{
-			contenedor[i]->Bind();
-			contenedor[i]->m_RealYPos += y;
-			contenedor[i]->m_YAxisMovement = glm::mat4(1.0f);
-			contenedor[i]->m_YAxisMovement = glm::translate(contenedor[i]->m_YAxisMovement, glm::vec3(contenedor[i]->m_RealXPos, contenedor[i]->m_RealYPos, contenedor[i]->m_RealZPos));
-			glUniformMatrix4fv(glGetUniformLocation(contenedor[i]->m_ProgramID, "move"), 1, GL_FALSE, &contenedor[i]->m_YAxisMovement[0][0]);
-		}
-		m_CurrentYPos += y;
-	}
-
-	void Shader::MoveDown(std::vector<Shader*>& contenedor, float y)
-	{
-		for (int i = 0; i < contenedor.size(); i++)
-		{
-			contenedor[i]->Bind();
-			contenedor[i]->m_RealYPos += y;
-			contenedor[i]->m_YAxisMovement = glm::mat4(1.0f);
-			contenedor[i]->m_YAxisMovement = glm::translate(contenedor[i]->m_YAxisMovement, glm::vec3(contenedor[i]->m_RealXPos, contenedor[i]->m_RealYPos, contenedor[i]->m_RealZPos));
-			glUniformMatrix4fv(glGetUniformLocation(contenedor[i]->m_ProgramID, "move"), 1, GL_FALSE, &contenedor[i]->m_YAxisMovement[0][0]);
-		}
-		m_CurrentYPos -= y;
-	}
-
-	void Shader::MoveShaderAxisY(float y)
-	{
-		this->Bind();
-		this->m_RealYPos += y;
-		this->m_YAxisMovement = glm::mat4(1.0f);
-		this->m_YAxisMovement = glm::translate(this->m_YAxisMovement, glm::vec3(this->m_RealXPos, this->m_RealYPos, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(this->m_ProgramID, "move"), 1, GL_FALSE, &this->m_YAxisMovement[0][0]);
-	}
-
-	void Shader::MoveShaderRight(float x)
-	{
-		this->Bind();
-		this->m_RealXPos += x;
-		this->m_XAxisMovement = glm::mat4(1.0f);
-		this->m_XAxisMovement = glm::translate(this->m_XAxisMovement, glm::vec3(this->m_RealXPos, this->m_RealYPos, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(this->m_ProgramID, "move"), 1, GL_FALSE, &this->m_XAxisMovement[0][0]);
-	}
-	
-	void Shader::MoveShaderLeft(float x)
-	{
-		this->Bind();
-		this->m_RealXPos -= x;
-		this->m_XAxisMovement = glm::mat4(1.0f);
-		this->m_XAxisMovement = glm::translate(this->m_XAxisMovement, glm::vec3(this->m_RealXPos, this->m_RealYPos, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(this->m_ProgramID, "move"), 1, GL_FALSE, &this->m_XAxisMovement[0][0]);
-	}
-
-	void Shader::SetPos(glm::vec3 vecPos)
-	{
-		Bind();
-		m_CurrentYPos = vecPos.y;
-		m_CurrentXPos = vecPos.x;
-		m_CurrentZPos = vecPos.z;
-		m_RealYPos = vecPos.y;
-		m_RealXPos = vecPos.x;
-		m_RealZPos = vecPos.z;
-		m_SetPos = glm::mat4(1.0f);
-		m_SetPos = glm::translate(m_SetPos, vecPos);
-		glUniformMatrix4fv(glGetUniformLocation(this->m_ProgramID, "move"), 1, GL_FALSE, &this->m_SetPos[0][0]);
 	}
 
 } 
--- SE/Source/Graphics/Shader.h
+++ SE/Source/Graphics/Shader.h
@@ -15,45 +15,19 @@ namespace se {
 		void Bind() const;
 		void UnBind() const;
 
-		void SetUniform(Shader& shader, const char* transformName, glm::mat4& matrix);
-		void AddShader(std::vector<Shader*>& contenedor);
-	public:
-		void MoveRight	(std::vector<Shader*>& contenedor, float x);
-		void MoveLeft	(std::vector<Shader*>& contenedor, float x);
-		void MoveUp		(std::vector<Shader*>& contenedor, float y);
-		void MoveDown	(std::vector<Shader*>& contenedor, float y);
-
-		void MoveShaderAxisY(float y);
-		void MoveShaderRight(float x);
-		void MoveShaderLeft(float x);
-
-		void SetPos(glm::vec3 vecPos);
-		float GetPosX() const { return m_CurrentXPos; }
-		float GetPosY() const { return m_CurrentYPos; }	
-
-		float GetRealPosX() const { return m_RealXPos; };
-		float GetRealPosY() const { return m_RealYPos; };
-
 		uint32_t GetProgramID() const { return m_ProgramID; }
+
+		void SetUniform(Shader& shader, const char* transformName, glm::mat4& matrix);
 	private:
-		glm::mat4 m_DefaultPos;
-		glm::mat4 m_XAxisMovement;
-		glm::mat4 m_YAxisMovement;
-
-		glm::mat4 m_SetPos;
-		float m_RealXPos;
-		float m_RealYPos;
-		float m_RealZPos;
-		float m_CurrentXPos;
-		float m_CurrentYPos;
-		float m_CurrentZPos;
-
 		uint32_t m_ProgramID;
 		uint32_t m_VertexID, m_FragmentID;
+
 		const char* m_VShaderCode;
 		const char* m_FShaderCode;
+
 		int success;
 		char infoLog[512];
+
 		std::string m_VertexCode;
 		std::string m_FragmentCode;
 		std::ifstream m_VShaderFile;
