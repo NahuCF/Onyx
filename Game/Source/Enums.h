8ab488c0fd1477dd2a4d4221a987ac73ddcd8bd8
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-10-08 18:44:53 -0300

    Added Enum.h

---
+++ Game/Source/Enums.h
@@ -0,0 +1,32 @@
+#pragma once
+
+enum class CommodityType
+{
+	Food,
+	Stone,
+	Wood,
+	Gold
+};
+
+enum class EntityType
+{
+	Villager
+};
+
+enum class ActionType
+{
+	Move,
+	Attack,
+	Gather
+};
+
+enum class MapSize
+{
+    Tiny = 72
+};
+
+enum class EventType
+{
+    Processing,
+    Success
+};
