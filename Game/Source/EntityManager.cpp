6820cafc9951033a022fe6a8efc34613a5507135
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-10-10 11:25:44 -0300

    Added EntityManager

--- Game/Source/EntityManager.cpp
+++ Game/Source/EntityManager.cpp
@@ -1,5 +1,6 @@
 #include "EntityManager.h"
 #include "Enums.h"
+#include "Actions/ActionMove.h"
 
 EntityManager::EntityManager(uint32_t entityCount, std::shared_ptr<se::Renderer2D> renderer)
     : m_Renderer(renderer)
@@ -23,4 +24,53 @@ void EntityManager::Remove(const std::shared_ptr<Entity>& entity)
     if(it != m_Entities.end()) {
         m_Entities.erase(it);
     }
+}
+
+void EntityManager::AddActionToSelected(ActionPtr action)
+{
+    EntityVector::iterator it;
+    for (it = m_SelectedEntities.begin(); it != m_SelectedEntities.end(); it++) {
+        (*it)->AddAction(action);
+    }
+}
+
+void EntityManager::SetSelected(lptm::Vector2D begin, lptm::Vector2D end,std::shared_ptr<Entity> entity)
+{
+    EntityVector::iterator it;
+    for(it = m_Entities.begin(); it != m_Entities.end(); it ++ ){
+        (*it)->SetColor({1.0f, 0.0, 0.0f, 1.0f});
+    }
+
+    m_SelectedEntities.clear();
+
+    lptm::Vector3D cameraPosition = m_Renderer.lock()->GetCamera().lock()->GetPosition();
+    begin = begin - lptm::Vector2D(cameraPosition.x, cameraPosition.y);
+    end = end - lptm::Vector2D(cameraPosition.x, cameraPosition.y);
+
+    //EntityVector::iterator it;
+    for(it = m_Entities.begin(); it != m_Entities.end(); it ++ ){
+        lptm::Vector2D entityPosition = (*it)->GetPosition();
+
+        if(entityPosition.x > begin.x && entityPosition.x < end.x
+        && entityPosition.y > begin.y && entityPosition.y < end.y)
+        {
+            (*it)->SetColor({0.0f, 0.0, 1.0f, 1.0f});
+            m_SelectedEntities.push_back(*it);
+        }
+
+    }
+}
+
+void EntityManager::OnRightClick(lptm::Vector2D normalizedMousePos)
+{
+    // Move
+    EntityVector::iterator it;
+    for(it = m_SelectedEntities.begin(); it != m_SelectedEntities.end(); it ++ ){
+        MoveUnitTo(*it, normalizedMousePos);
+    }
+}
+
+void EntityManager::MoveUnitTo(std::shared_ptr<Entity>& entity, lptm::Vector2D destination)
+{
+   entity->AddAction(ActionMove::MoveTo(destination, entity));
 }
--- Game/Source/EntityManager.h
+++ Game/Source/EntityManager.h
@@ -16,7 +16,15 @@ public:
 
     void Add(const std::shared_ptr<Entity>& entity) { m_Entities.push_back(entity); }
     void Remove(const std::shared_ptr<Entity>& entity);
+
+    void AddActionToSelected(ActionPtr action);
+    void SetSelected(lptm::Vector2D begin, lptm::Vector2D end,std::shared_ptr<Entity> entity);
+
+    void OnRightClick(lptm::Vector2D normalizedMousePos);
+
+    void MoveUnitTo(std::shared_ptr<Entity>& entity, lptm::Vector2D destination);
 private:
     EntityVector m_Entities;
+    EntityVector m_SelectedEntities;
     std::weak_ptr<se::Renderer2D> m_Renderer;
 };
