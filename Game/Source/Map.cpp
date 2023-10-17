9b1c7b52cb26f6fbe27f6a85596048943cc8e454
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-10-17 15:56:31 -0300

    Added SetExploredInRange method in Map.h

--- Game/Source/Map.cpp
+++ Game/Source/Map.cpp
@@ -1,8 +1,39 @@
 #include "Map.h"
 
 Map::Map(uint32_t cols, uint32_t rows)
+    : m_Cols(cols)
+    , m_Rows(rows)
+    
 {
-    m_TileData.resize(cols * rows);
+    TileData a;
+    m_TileData = std::vector<TileData>(cols * rows, a);
+}
+
+void Map::SetExploredInRange(lptm::Vector2D origin, int tileVision)
+{
+    for(int y = -tileVision; y <= tileVision; y++)
+    {
+        for(int x = -tileVision; x <= tileVision; x++)
+        {
+            int32_t posX = (int)origin.x + x; 
+            int32_t posY = (int)origin.y + y; 
+
+            uint32_t index = posY * m_Rows + posX;
+
+            if(index >= m_TileData.size())
+                continue;   
+
+            m_TileData[index].IsExplored = true;
+
+            //ImGui::Text("PosX: %i PosY: %i Index: %i, Origin: %i, %i", x, y, index, (int)origin.x, (int)origin.y);
+        }
+    }
+}
+
+void Map::CleanExplored()
+{
+    for(auto& tile : m_TileData) 
+        tile.IsExplored = false;
 }
 
 void Map::AddEntityAt(int col, int row, lptm::Vector2D mapSize, Entity& entity)
--- Game/Source/Map.h
+++ Game/Source/Map.h
@@ -16,9 +16,17 @@ public:
 
     TileData GetTileData(uint32_t index) { return m_TileData[index]; }
 
+    uint32_t GetTileCount() const { return m_TileData.size();}
+
+    void SetExploredInRange(lptm::Vector2D tileOrigin, int tileVision);
+    void CleanExplored();
+
     void AddEntityAt(int col, int row, lptm::Vector2D mapSize, Entity& entity);
     void DeleteEntityAt(int col, int row, lptm::Vector2D mapSize, Entity& entity);
 private:
     std::vector<std::vector<Entity>> m_TileEntities;
     std::vector<TileData> m_TileData;
+
+    uint32_t m_Cols;
+    uint32_t m_Rows;
 };
