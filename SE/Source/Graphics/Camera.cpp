bcae162cb16f7dba8b9138fe1c091a29728b03bb
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-10-18 12:33:40 -0300

    Added Camera class

---
+++ SE/Source/Graphics/Camera.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+#include "Camera.h"
+
+namespace se {
+    Camera::Camera()
+    {
+
+    }
+}
---
+++ SE/Source/Graphics/Camera.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Maths/Maths.h"
+
+namespace se {
+    class Camera
+    {
+    public:
+        Camera();
+
+        void SetPosition(lptm::Vector3D vector) { m_Position = { -vector.x, -vector.y, -vector.z }; }
+        void MoveCamera(lptm::Vector3D vector) { m_Position += { vector.x, vector.y, vector.z }; }
+
+        lptm::Vector3D GetPosition() const { return m_Position; }
+    private:
+        lptm::Vector3D m_Position;
+    };
+}
