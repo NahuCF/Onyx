048804a7fcd7efa73563869d00f0e967245e0570
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-11-21 23:09:19 -0300

    Added operator << to allow chaining data in Message.h

--- Onyx/Source/Networking/Message.h
+++ Onyx/Source/Networking/Message.h
@@ -17,10 +17,30 @@ namespace Onyx {
         Header<T> header;
         std::vector<uint8_t> data; 
 
+        // Return the size of the message in bytes
         size_t Size() const
         {
             return sizeof(Header<T>) + data.size();
         };
+
+        template<typename DataType>
+        friend Message<T>& operator<<(Message<T>& message, DataType& data)
+        {
+            // Cache current size vector, as this will be the point where we insert data
+            size_t sizeData = message.size();
+
+            // Resize vector to hold new data
+            message.data.resize(message.data.size() + sizeof(DataType));
+
+            // Copy the data into the newly resized vector
+            std::memcpy(message.data + sizeData, data, sizeof(DataType));
+
+            // Update size of message
+            message.header.size = message.Size();
+
+            // Return the message so it can be chained
+            return message;
+        }
     };
 
 }
