2e8937ae544fdba18fbbf4d440011f2c629b0fec
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-11-22 19:41:39 -0300

    Added >> operator in Mesasge.h

--- Onyx/Source/Networking/Message.h
+++ Onyx/Source/Networking/Message.h
@@ -27,13 +27,32 @@ namespace Onyx {
         friend Message<T>& operator<<(Message<T>& message, DataType& data)
         {
             // Cache current size vector, as this will be the point where we insert data
-            size_t sizeData = message.size();
+            size_t sizeData = message.Size();
 
             // Resize vector to hold new data
             message.data.resize(message.data.size() + sizeof(DataType));
 
             // Copy the data into the newly resized vector
-            std::memcpy(message.data + sizeData, data, sizeof(DataType));
+            std::memcpy(message.data.data() + sizeData, &data, sizeof(DataType));
+
+            // Update size of message
+            message.header.size = message.Size();
+
+            // Return the message so it can be chained
+            return message;
+        }
+
+        template<typename DataType>
+        friend Message<T>& operator<<(Message<T>& message, DataType& data)
+        {
+            // Cache current size vector, as this will be the point where we insert data
+            size_t sizeData = message.Size() - sizeof(DataType);
+
+            // Copy the data into the newly resized vector
+            std::memcpy(&data, message.data.data() + sizeData, sizeof(DataType));
+
+            // Resize vector to hold new data
+            message.data.resize(message.data.size() + sizeof(DataType));
 
             // Update size of message
             message.header.size = message.Size();
