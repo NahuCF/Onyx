9ef58134ecdcc2f880501946e804bb675c26574c
Nahuel <58460486+NahuCF@users.noreply.github.com>
2023-12-14 19:12:42 -0300

    Add definition for Camera class

--- Onyx/Source/Graphics/Camera.cpp
+++ Onyx/Source/Graphics/Camera.cpp
@@ -3,8 +3,23 @@
 #include "Camera.h"
 
 namespace Onyx {
-    Camera::Camera()
-    {
 
+    Camera::Camera(int width, int height, glm::vec3 position)
+        : m_Width(width)
+        , m_Height(height)
+        , m_Position(position)
+    {
     }
+
+    void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader &shader, const char *uniform)
+    {
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+
+        view = glm::lookAt(m_Position, m_Position + m_Orientation, m_Up);
+        projection = glm::perspective(glm::radians(FOVdeg),(float)(m_Width / m_Height), nearPlane, farPlane); 
+
+        glUniformMatrix4fv(glGetUniformLocation(shader.GetProgramID(), uniform), 1, GL_FALSE, glm::value_ptr(projection));
+    }
+
 }
