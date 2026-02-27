3f90f355d36f24e6e772ccad78f3c8d949069d19
Nahuel <nahuefer173@gmail.com>
2026-02-27 23:11:04 +0000

    feat: add thread safe texture load

--- Onyx/Source/Graphics/Texture.cpp
+++ Onyx/Source/Graphics/Texture.cpp
@@ -39,6 +39,45 @@ namespace Onyx {
 		return texture;
 	}
 
+	PreloadedImage Texture::PreloadFromFile(const char* path) {
+		PreloadedImage result;
+		stbi_set_flip_vertically_on_load_thread(1);
+		unsigned char* data = stbi_load(path, &result.width, &result.height, &result.channels, 0);
+		if (data) {
+			size_t size = static_cast<size_t>(result.width) * result.height * result.channels;
+			result.pixels.assign(data, data + size);
+			stbi_image_free(data);
+		}
+		return result;
+	}
+
+	std::unique_ptr<Texture> Texture::CreateWithMipmaps(const void* data, int width, int height, int channels) {
+		auto texture = std::unique_ptr<Texture>(new Texture());
+		texture->m_TextureWidth = width;
+		texture->m_TextureHeight = height;
+		texture->m_NChannels = channels;
+
+		glGenTextures(1, &texture->m_TextureID);
+		texture->Bind();
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		GLenum internalFormat, dataFormat;
+		if (channels == 4) { internalFormat = GL_RGBA8; dataFormat = GL_RGBA; }
+		else if (channels == 3) { internalFormat = GL_RGB8; dataFormat = GL_RGB; }
+		else if (channels == 1) { internalFormat = GL_R8; dataFormat = GL_RED; }
+		else { internalFormat = GL_RGBA8; dataFormat = GL_RGBA; }
+
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		texture->UnBind();
+		return texture;
+	}
+
 	void Texture::InitFromData(const void* data, int width, int height, int channels)
 	{
 		m_TextureWidth = width;
@@ -97,8 +136,6 @@ namespace Onyx {
 	void Texture::InitTexture(const char* texturePath, bool useColorKey, uint8_t keyR, uint8_t keyG, uint8_t keyB)
 	{
 		stbi_set_flip_vertically_on_load(true);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		glGenTextures(1, &m_TextureID);
 		Bind();
@@ -169,6 +206,7 @@ namespace Onyx {
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		stbi_image_free(m_TextureData);
+		m_TextureData = nullptr;
 		if (rgbaData) {
 			delete[] rgbaData;
 		}
@@ -198,7 +236,10 @@ namespace Onyx {
 
     void Texture::SetData(void* data)
     {
-        glTextureSubImage2D(this->m_TextureID, 0, 0, 0, m_TextureWidth, m_TextureHeight, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        GLenum format = GL_RGBA;
+        if (m_NChannels == 3) format = GL_RGB;
+        else if (m_NChannels == 1) format = GL_RED;
+        glTextureSubImage2D(this->m_TextureID, 0, 0, 0, m_TextureWidth, m_TextureHeight, format, GL_UNSIGNED_BYTE, data);
     }
 
 std::unique_ptr<Texture> Texture::CreateFloatTexture(const float* data, int width, int height)
--- Onyx/Source/Graphics/Texture.h
+++ Onyx/Source/Graphics/Texture.h
@@ -2,8 +2,16 @@
 
 #include "Maths/Maths.h"
 #include <memory>
+#include <vector>
 
 namespace Onyx {
+
+	struct PreloadedImage {
+		std::vector<unsigned char> pixels;
+		int width = 0, height = 0, channels = 0;
+		bool Valid() const { return !pixels.empty(); }
+		void Free() { pixels.clear(); pixels.shrink_to_fit(); }
+	};
 
 	class Texture
 	{
@@ -14,8 +22,11 @@ namespace Onyx {
 
 		static std::unique_ptr<Texture> CreateSolidColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
 		static std::unique_ptr<Texture> CreateFromData(const void* data, int width, int height, int channels);
+		static std::unique_ptr<Texture> CreateWithMipmaps(const void* data, int width, int height, int channels);
 		static std::unique_ptr<Texture> CreateFloatTexture(const float* data, int width, int height);
 		static std::unique_ptr<Texture> CreateNoiseTexture(const float* data, int width, int height);
+
+		static PreloadedImage PreloadFromFile(const char* path);
 
 		void Bind() const;
 		void Bind(uint32_t slot) const;
@@ -35,9 +46,9 @@ namespace Onyx {
 		void InitSolidColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 		void InitFromData(const void* data, int width, int height, int channels);
 
-		uint32_t m_TextureID;
-		unsigned char* m_TextureData;
-		int m_TextureWidth, m_TextureHeight, m_NChannels;
+		uint32_t m_TextureID = 0;
+		unsigned char* m_TextureData = nullptr;
+		int m_TextureWidth = 0, m_TextureHeight = 0, m_NChannels = 0;
 	};
 
 }
